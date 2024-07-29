29de08231319512b0b5b912ed8821cf1fc325e91
Aiden Storey <git@storey.dev>
2024-07-29 00:09:32 -0400

    Add triangles, meshes, and line rasterization

--- src/display.c
+++ src/display.c
@@ -65,6 +65,25 @@ void draw_grid(void) {
     }
 }
 
+void draw_line(int x0, int y0, int x1, int y1, uint32_t color) {
+    int delta_x = x1 - x0;
+    int delta_y = y1 - y0;
+
+    int side_length = abs(delta_x) >= abs(delta_y) ? abs(delta_x) : abs(delta_y);
+
+    float x_inc = delta_x / (float) side_length;
+    float y_inc = delta_y / (float) side_length;
+
+    float current_x = x0;
+    float current_y = y0;
+
+    for (int i = 0; i <= side_length; ++i) {
+        draw_pixel(round(current_x), round(current_y), color);
+        current_x += x_inc;
+        current_y += y_inc;
+    }
+}
+
 void draw_rect(int x, int y, int width, int height, uint32_t color) {
     for (int dx = 0; dx < width; ++dx) {
         for (int dy = 0; dy < height; ++dy) {
@@ -73,17 +92,12 @@ void draw_rect(int x, int y, int width, int height, uint32_t color) {
     }
 }
 
-// void draw_rect(int x, int y, int width, int height, uint32_t color) {
-//     for (int dx = x; dx < x + width; ++dx) {
-//         color_buffer[y * window_width + dx] = color;
-//         color_buffer[(y + height) * window_width + dx] = color;
-//     }
+void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
+    draw_line(x0, y0, x1, y1, color);
+    draw_line(x1, y1, x2, y2, color);
+    draw_line(x2, y2, x0, y0, color);
+}
 
-//     for (int dy = y; dy < y + height; ++dy) {
-//         color_buffer[dy * window_width + x] = color;
-//         color_buffer[dy * window_width + (x + width)] = color;
-//     }
-// }
 
 void render_color_buffer(void) {
     SDL_UpdateTexture(
